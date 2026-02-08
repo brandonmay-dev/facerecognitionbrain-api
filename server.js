857591a4c0f5f916b4105ba743fd16986620f1ea
Marqui <bmay86@protonmail.com>
2026-02-08 12:03:23 -0500

    Add listen method to start server on port 3000

--- server.js
+++ server.js
@@ -3,3 +3,7 @@ const http = require('http');
 const server = http.createServer(() => {
     console.log('server is running');
 }
+
+server.listen(3000, () => {
+    console.log('server is listening on port 3000');
+}
