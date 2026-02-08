56d94346e4984c9a779179d1135030706901ff22
Marqui <bmay86@protonmail.com>
2026-02-08 12:07:48 -0500

    Return plain text response from HTTP server

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
-const http = require('http');
+const http = require("http");
 
-const server = http.createServer(() => {
-    console.log('server is running');
-}
+const server = http.createServer((req, res) => {
+  res.setHeader("Content-Type", "text/plain");
+  res.end("Hello, World!");
+});
 
 server.listen(3000, () => {
-    console.log('server is listening on port 3000');
-}
+  console.log("server is listening on port 3000");
+});
