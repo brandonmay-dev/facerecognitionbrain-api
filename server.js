afd6f64178b9863fb06edabbdd5db92f663ce701
Marqui <bmay86@protonmail.com>
2026-02-08 20:40:12 -0500

    Create mock users data store

--- server.js
+++ server.js
@@ -3,6 +3,27 @@ import bodyParser from "body-parser";
 
 const app = express();
 
+const database = {
+  users: [
+    {
+      id: "123",
+      name: "John",
+      email: "john@example.com",
+      password: "cookies",
+      entries: 0,
+      joined: new Date(),
+    },
+    {
+      id: "124",
+      name: "Sally",
+      email: "sally@example.com",
+      password: "bananas",
+      entries: 0,
+      joined: new Date(),
+    },
+  ],
+};
+
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
