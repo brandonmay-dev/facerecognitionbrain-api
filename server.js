005d65d52e8dce6629e5590060469d3b8f539243
Marqui <bmay86@protonmail.com>
2026-02-08 20:35:01 -0500

    Add /signin POST route to Express server

--- server.js
+++ server.js
@@ -6,6 +6,11 @@ const app = express();
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
+
+app.post("/signin", (req, res) => {
+  res.json("signing in");
+});
+
 app.listen(3000, () => {
   console.log("app is running on port 3000");
 });
