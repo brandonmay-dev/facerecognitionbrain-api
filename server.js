49a6ed5aa66897a642130212a6bf944ba8739c55
Marqui <bmay86@protonmail.com>
2026-02-23 01:49:06 -0500

    Add GET /profile/:id route to fetch user by ID from database

--- server.js
+++ server.js
@@ -174,6 +174,21 @@ app.put("/image", async (req, res) => {
   }
 });
 
+app.get("/profile/:id", (req, res) => {
+  const { id } = req.params;
+  db.select("*")
+    .from("users")
+    .where({ id })
+    .then((user) => {
+      if (user.length) {
+        res.json(user[0]);
+      } else {
+        res.status(400).json("Not found");
+      }
+    })
+    .catch((err) => res.status(400).json("error getting user"));
+});
+
 app.listen(PORT, () => {
   console.log(`✅ Server listening on http://localhost:${PORT}`);
 });
